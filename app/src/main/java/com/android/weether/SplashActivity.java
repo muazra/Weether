b9ef44cf7aa8c46f7013e63d2c4c9b09cb74fd8f
Muaz Rahman <muazra@gmail.com>
2014-10-05 03:59:17 -0400

    current location discovery added

--- app/src/main/java/com/android/weether/SplashActivity.java
+++ app/src/main/java/com/android/weether/SplashActivity.java
@@ -1,36 +1,60 @@
 package com.android.weether;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class SplashActivity extends Activity {
+
+    private static final String TAG = "SplashActivity";
+    LocationManager mlocationManager;
+    LocationListener mlocationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        mlocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mlocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                List<Address> address = findGeoCode(location);
+                Log.d(TAG, "Current city = " + address.get(0).getLocality());
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {}
+            @Override
+            public void onProviderEnabled(String s) {}
+            @Override
+            public void onProviderDisabled(String s) {}
+        };
+
+        mlocationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, mlocationListener, null);
+
     }
 
+    protected List<Address> findGeoCode(Location location){
+        Geocoder geoCoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        try {
+            List<Address> address = geoCoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            if (address.size() > 0) {
+                return address;
+            }
+        }catch(IOException e){}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.splash, menu);
-        return true;
+        return null;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
